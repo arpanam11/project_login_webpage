097680f577d25374de1dfdb9d314c3045735c4ce
arpanam11 <arpanameshram11@gmail.com>
2024-07-10 16:08:18 +0530

    create component for login

---
+++ myproject/src/component/Login.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+const [username , setUsername]= useState('')
+const [password , setPassword]= useState('')
+
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
+
+export default Login
