6140baaf28dc7b6bad611a08977bcc1e64e5a2b8
arpanam11 <arpanameshram11@gmail.com>
2024-07-17 20:04:18 +0530

    login page

--- myproject/src/component/Login.js
+++ myproject/src/component/Login.js
@@ -7,8 +7,8 @@ const [password , setPassword]= useState('')
   return (
     <div>
     <div className='landing_head'>a</div>
-    <div className='landing_h'>a</div>
-    <div className='landing_window'>a</div>
+    <div className='landing_h'>Login</div>
+    <div className='landing_window'>From </div>
     </div>
   )
 }
