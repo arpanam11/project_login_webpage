c5228246683f46187c3466ea6bb9601005b72955
arpanam11 <arpanameshram11@gmail.com>
2024-07-22 12:31:06 +0530

    code commit for routing

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Homepage from './component/Homepage';
 import Login from './component/Login';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-     <Homepage/>
-     <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
