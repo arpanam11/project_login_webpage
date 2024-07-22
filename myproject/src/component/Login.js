a9c5ba9df167614305b56724fb0ce4592210646f
arpanam11 <arpanameshram11@gmail.com>
2024-07-22 14:25:54 +0530

    add ed json server

--- myproject/src/component/Login.js
+++ myproject/src/component/Login.js
@@ -1,16 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../style/login.css';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-const [username , setUsername]= useState('')
-const [password , setPassword]= useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = async (e) => {
+    const navigate = useNavigate();
+    e.preventDefault();
+    console.log({ username, password});
+    const empData = { username, password };
+    try {
+      await axios.post('http://localhost:5000/users', empData);
+      navigate('/home');
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <div>
-    <div className='landing_head'>a</div>
-    <div className='landing_h'>Login</div>
-    <div className='landing_window'>From </div>
+      <div className='landing_head'>a</div>
+      <div className='landing_h'>Login</div>
+      <div className='landing_window'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input type='text' value={username} onChange={handleUsernameChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type='password' value={password} onChange={handlePasswordChange} />
+          </label>
+          <br />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
