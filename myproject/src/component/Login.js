d168b3ca5b4515472e4a98f7e29affbe7a9f72f4
arpanam11 <arpanameshram11@gmail.com>
2024-07-25 18:33:33 +0530

    commit code for login page

--- myproject/src/component/Login.js
+++ myproject/src/component/Login.js
@@ -5,7 +5,7 @@ import { useNavigate, Link } from 'react-router-dom';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState(''); // State for error message
+  const [errorMessage, setErrorMessage] = useState(''); 
   const navigate = useNavigate();
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -15,51 +15,49 @@ const Login = () => {
     setPassword(event.target.value);
   };
   const validateCredentials = () => {
-    // Validation logic
+  
     if (username.trim() === '') {
       setErrorMessage('Username cannot be empty.');
-      return false; // Prevent submission if validation fails
+      return false; 
     }
     if (password.trim() === '') {
       setErrorMessage('Password cannot be empty.');
       return false;
     }
 
-    // Assuming you have the provided `db.json` data
     const validUser = {
       username: 'Admin',
       password: 'Admin@2024'
-    }; // Or fetch user data from your backend
+    }; 
 
     if (username !== validUser.username || password !== validUser.password) {
       setErrorMessage('Invalid username or password.');
       return false;
     }
 
-    return true; // Valid credentials
+    return true; 
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!validateCredentials()) {
-      return; // Don't proceed if validation fails
+      return; 
     }
 
-    setErrorMessage(''); // Clear error message before API call
+    setErrorMessage(''); 
 
     try {
       const response = await axios.get('http://localhost:5000/users', {
         username,
         password,
       });
-      console.log('Login successful:', response.data); // Assuming response contains success data
+      console.log('Login successful:', response.data); 
       navigate('/home');
-      // Handle successful login (e.g., redirect, display success message)
 
     } catch (err) {
       console.error(err.message);
-      setErrorMessage('Login failed. Please try again.'); // Generic error message
+      setErrorMessage('Login failed. Please try again.'); 
     }
   };
  
@@ -88,7 +86,6 @@ const Login = () => {
           <button type="submit" class="btn btn-primary">Submit</button>
           <div className='d-flex'>
 
-            {/* ... form fields and logic */}
             <Link to="/register" >Register</Link>
 
            
