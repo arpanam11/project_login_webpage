aa09a1b51620012e280c0d6af06a9d61e6aaeb8b
arpanam11 <arpanameshram11@gmail.com>
2024-07-30 16:09:10 +0530

    create project page

---
+++ myproject/src/component/Project.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Homepage from './Homepage'
+
+const Project = () => {
+  return (
+    <div>
+        <Homepage>
+            <div>
+                hi
+            </div>
+        </Homepage>
+    </div>
+  )
+}
+
+export default Project
--- myproject/src/component/Section.js
+++ myproject/src/component/Section.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Section = () => {
   return (
-    <div className="cardSection m-5">Section</div>
+    <div className="cardSection m-5">
+     <div className="row">
+        <div className='col-lg-5 col-md-5 col-sm flex-container'>
+          
+        </div>
+        <div className='col-lg-5 col-md-5 col-sm flex-container'>
+
+        </div>
+     </div>
+    </div>
   )
 }
 
