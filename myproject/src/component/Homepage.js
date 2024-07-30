5ae716617c4932c3f9df363e87801b78dd28ca32
arpanam11 <arpanameshram11@gmail.com>
2024-07-30 16:34:43 +0530

    changes in home page

--- myproject/src/component/Homepage.js
+++ myproject/src/component/Homepage.js
@@ -1,73 +1,47 @@
-import React, { useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
-import '../style/content.css'
-import {  Link } from 'react-router-dom';
-import Section from './Section';
-const { Header, Sider, Content } = Layout;
+import React, { useState, useEffect } from 'react';
+import '../style/homepage.css'
+import axios from 'axios';
 
 const Homepage = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
+  const [projectdata, setprojectdata] = useState([])
 
- 
+  useEffect(() => {
+    getProjectsite()
+  }, [])
+
+
+  const getProjectsite = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/project');
+      setprojectdata(response.data)
+      console.log('Login successful:', response.data);
+
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="light"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} className='d-flex justify-content-between'>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-          <Button className='mr-5 mt-3 btn btn-info'><Link to="/"  className='p-2'>Logout</Link></Button>
-        </Header>
-        <Content
-       className='content'>
-      <Section/>
 
-        </Content>
-      </Layout>
-    </Layout>
-  );
-};
+    <div className="row">
+      <div className='col-lg-5 col-md-5 col-sm flex-container'>
+        <div className="cardBox">
+          {projectdata.map((item, index) => (
+            <div key={index}>
+              {
+                <p>{item.projectName}</p>
+              }
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='col-lg-5 col-md-5 col-sm flex-container'>
+
+      </div>
+    </div>
+
+  )
+}
+
 
 export default Homepage;
