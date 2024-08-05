34f5df3c8e094e1f99a388075e687290b4eb194d
arpanam11 <arpanameshram11@gmail.com>
2024-08-05 21:03:13 +0530

    home page

--- myproject/src/common/Navbar.js
+++ myproject/src/common/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
          
         </div>
         <ul className="nav navbar-nav">
-          <li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
+          <li className="nav-item active"><a className="nav-link" href="#">Home Page</a></li>
           {/* Add more nav items as needed */}
         </ul>
       </div>
